741fc3310c861a526eb25364be7c8596043dbdee
felipegontijo <felipezeiro@rrrrrrrrr.local>
2019-06-21 12:10:58 -0400

    add flip card functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*
  * Create a list that holds all of your cards
- */
+*/
 
 
 /*
@@ -8,7 +8,7 @@
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
- */
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -35,4 +35,21 @@ function shuffle(array) {
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+*/
+
+// get the entire deck of cards
+const deck = document.querySelector('.deck');
+
+// flips the card
+function flipCard (card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
+
+// adds an event listener to the deck
+deck.addEventListener('click', function(e) {
+    // defines card as what triggered the event
+    const card = e.target;
+    //flips the card
+    flipCard(card);
+})
